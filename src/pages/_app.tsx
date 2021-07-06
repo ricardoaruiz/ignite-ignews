d86574ec994b939be694abe1539a21c367ae8da0
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-07-06 20:43:09 -0300

    feat: create _document and add roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import { AppProps } from 'next/app'
 
+// Toda a página acessada será carregada por esse arquivos
+// portanto toda vez que uma página for carregada, tudo
+// que for colocado aqui será recriado (estados, chamadas a api e etc)
+// Esse arquivo seria o App.tsx no SPA
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
---
+++ src/pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+// Esse arquivo será carregado uma única vez para a aplicação
+// bom lugar para colocar coisas que devem ser compartilhadas
+// Esse arquivo seria o index.html no SPA
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
