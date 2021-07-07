12158967d63e016e22784f16fe48efc3a8a83f28
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-07-06 21:01:35 -0300

    feat: add global style in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import { AppProps } from 'next/app'
-
 // Toda a página acessada será carregada por esse arquivos
 // portanto toda vez que uma página for carregada, tudo
 // que for colocado aqui será recriado (estados, chamadas a api e etc)
 // Esse arquivo seria o App.tsx no SPA
+
+import { AppProps } from 'next/app'
+
+import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
