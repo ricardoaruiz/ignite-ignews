5f5ae089c7ad4d94e3f1a5334c75bb5d1ed1d500
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-07-07 10:05:08 -0300

    feat: create header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,17 @@
 // Esse arquivo seria o App.tsx no SPA
 
 import { AppProps } from 'next/app'
+import { Header } from 'components/Header'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
