12ec44b9c897d78c5b393b391bdac5241cc9f265
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-07-08 11:12:44 -0300

    feat: add faunadb sdk configure it and save logged user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+import { fauna } from 'services/fauna'
 
 // https://next-auth.js.org/getting-started/introduction
 // https://next-auth.js.org/getting-started/example
@@ -14,8 +16,28 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user',
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email: user.email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+          )
+        )
+        return true
+      } catch (error) {
+        console.log('error', error)
+        return false
+      }
+    },
+  },
 
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
